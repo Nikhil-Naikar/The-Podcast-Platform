91c2c7271d0c698f0d665a246547b513b1e8b13b
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2024-08-03 15:26:31 -0400

    added clerk package, added http.ts & auth.config.ts

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,12 @@
+import { SignIn } from '@clerk/clerk-react'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex-center glassmorphism-auth h-screen w-full'>
+        <SignIn />
+    </div>
+  )
+}
+
+export default page
---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://whole-snapper-63.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+  
+  export default authConfig;
