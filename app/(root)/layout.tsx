76dd669c324beaf17f89c5f576726e83298b255e
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2024-08-03 11:52:59 -0400

    taking a mini break, quick recap of what i learned so far, file folder routing, client vs sever components, tailwind, how to organize project files and folders, like bit about design though process

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import LeftSideBar from "@/components/LeftSideBar";
+import MobileNav from "@/components/MobileNav";
 import RightSideBar from "@/components/RightSideBar";
 import Image from "next/image";
 
@@ -7,11 +8,11 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
    <div className="relative flex flex-col">
         <main className="relative flex bg-black-3">
             <LeftSideBar />
-              <section className="border-2 border-red-500 flex min-h-screen flex-1 flex-col px-4 sm:px-14">
+              <section className="border-2 flex min-h-screen flex-1 flex-col px-4 sm:px-14">
                 <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
                   <div className="flex h-16 items-center justify-between md:hidden">
                     <Image src="/icons/logo.svg" alt="menu icon" width={30} height={30}/>
-                    MobileNav
+                    <MobileNav/>
                   </div>
                   <div>
                     Toaster (notification popups)
