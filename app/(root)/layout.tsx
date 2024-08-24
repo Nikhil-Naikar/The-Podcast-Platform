59545771a6b81c8012a0b93b2ce317cff28dd367
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2024-08-24 19:18:42 -0400

    audio player complete

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,7 @@ import MobileNav from "@/components/MobileNav";
 import RightSideBar from "@/components/RightSideBar";
 import Image from "next/image";
 import { Toaster } from "@/components/ui/toaster";
+import PodcastPlayer from "@/components/PodcastPlayer";
 
 export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
   return (
@@ -18,11 +19,13 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
                   <div>
                     <Toaster />
                     {children}
+
                   </div>
                 </div>
               </section>
             <RightSideBar/>
         </main>
+        <PodcastPlayer />
    </div>
   );
 }
