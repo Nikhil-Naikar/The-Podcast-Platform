c97a4cd25a358c4961ab32cb227704f6f39a63ba
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2024-08-25 16:29:29 -0400

    building mobile nav bar

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import Image from "next/image"
+
 
 const MobileNav = () => {
   return (
-    <div>MobileNav</div>
+    <section>
+      <Sheet>
+        <SheetTrigger>
+          <Image src="/icons/hamburger.svg" width={30} height={30} alt="menu" className="cursor-pointer" />
+        </SheetTrigger>
+        <SheetContent side="left" className="border-none bg-black-1">
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your account
+              and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </section>
+
   )
 }
 
