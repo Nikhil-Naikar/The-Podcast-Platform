8e0ea115bbfef4681151895a15ab910ec7731d46
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2024-08-25 16:43:05 -0400

    cp:mobile nav bar complete

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,15 +1,20 @@
+"use client";
+
 import {
   Sheet,
+  SheetClose,
   SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
+import { sidebarLinks } from "@/constants"
+import { cn } from "@/lib/utils"
 import Image from "next/image"
-
+import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 const MobileNav = () => {
+  const pathname = usePathname();
+  
   return (
     <section>
       <Sheet>
@@ -17,13 +22,28 @@ const MobileNav = () => {
           <Image src="/icons/hamburger.svg" width={30} height={30} alt="menu" className="cursor-pointer" />
         </SheetTrigger>
         <SheetContent side="left" className="border-none bg-black-1">
-          <SheetHeader>
-            <SheetTitle>Are you absolutely sure?</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your account
-              and remove your data from our servers.
-            </SheetDescription>
-          </SheetHeader>
+          <Link href='/' className='flex cursor-pointer items-center gap-1 pb-10 pl-4'>
+              <Image src='/icons/logo.svg' alt='logo' width={23} height={27}/>
+              <h1 className='text-24 font-extrabold text-white-1 ml-2'>The Podcast Platform</h1>
+          </Link>
+          <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
+            <SheetClose asChild>
+              <nav className="flex h-full flex-col gap-6 text-white-1">
+                {sidebarLinks.map(({route,label,imgURL}) => {
+                  const isActive = pathname === route || pathname.startsWith(`${route}/`);
+
+                  return(
+                    <SheetClose asChild key={route}>
+                      <Link href={route} key={label} className={cn('flex gap-3 items-center py-4 max-lg:px-4 justify-start', { 'bg-nav-focus border-r-4 border-orange-1':isActive })}>
+                          <Image src={imgURL} alt={label} width={24} height={24}/>
+                          <p>{label}</p>
+                      </Link>
+                    </SheetClose>
+                  )
+                })}
+              </nav>
+            </SheetClose>
+          </div>
         </SheetContent>
       </Sheet>
     </section>
